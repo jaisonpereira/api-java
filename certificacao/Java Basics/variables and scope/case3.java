db9f748901761dbebb01f2fedd5f8fab402127db
Jaison Pereira <jaison@logusretail.com.br>
2017-11-08 14:28:15 -0200

    teste

---
+++ certificacao/Java Basics/variables and scope/case3.java
@@ -0,0 +1,19 @@
+public class case1 {
+static int x = 15;
+
+ public static void main(String[] x) {
+	 x[0] = "java cool";
+ 	System.out.println(x[0]);
+     }
+ }
+
+//1) O código compila mas dá erro em execução.
+//2) O código compila e roda, imprimindo java cool.
+//3) O código não compila.
+
+
+
+
+
+
+
